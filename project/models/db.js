e88b1f340fae9c325d7333e272be3b17d7bf70cd
MANISH KUMAR <72985740+manish-190@users.noreply.github.com>
2022-07-01 15:58:01 +0530

    Add files via upload

---
+++ project/models/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+require('./employee.model');
